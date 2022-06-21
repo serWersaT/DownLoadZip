cde332e40a48d32eb42891eab428e43699cc3d85
wersa <wersa@DESKTOP-UN89P4E>
2022-06-21 17:38:53 +0300

    update

--- DownLoadZip.BLL/Services/Service.cs
+++ DownLoadZip.BLL/Services/Service.cs
@@ -13,71 +13,79 @@ namespace DownLoadZip.BLL.Services
     public class Service
     {
         public string localpathdownload = @"D:\archive";
-        public long maxlength = 1024 * 100;
 
-        public int GetFiles(string path)
+        public int GetZipArchives(string path, int maxlengthzip, Dictionary<string, string> referens, string savepath)
         {
-            List<string> blockfiles = new List<string>();
-
-            long filesize = 0;
+            /*
+             тут Dictionary<string, string> referens условие взаимосвязи один (key) ко многи (value). 
+             В донном примере Key это файлы в папке Blog, а Value - данные в папке Article. 
+             */
             int numbername = 0;
-            bool addfile = true;
-            var str = "";
-
-            IEnumerable<string> Dir = Directory.GetFileSystemEntries(path, "*", SearchOption.AllDirectories).Where(x => x.Contains("."));
-            var Blogs = Dir.Where(x => x.Contains("Blog")).Select(c => c.Substring(c.LastIndexOf('\\') + 1).Replace(".json", "")).ToList();
-
+            long archivesize = 0;
+            List<string> Dir = new List<string>();
             try
             {
-
-                foreach (var file in Dir.OrderBy(x => x))
-                {
-                    filesize += new System.IO.FileInfo(file).Length;
-
-                    str = file.Replace(localpathdownload + @"\Article\", "");
-                    str = str.Substring(0, str.IndexOf('\\'));
-
-                    if (file.Contains("Article") && blockfiles.Any(x => x.Contains(str)) && file.Contains(str))
-                    {
-                        addfile = true;
-                    }
-                    else addfile = false;
-
-                    if (filesize >= maxlength && addfile == false)
-                    {
-                        GetArchive(blockfiles.Distinct().ToList(), "SuperZip_" + numbername.ToString());
-                        numbername++;
-                        filesize = 0;
-                        blockfiles.Clear();
-                    }
-
-                    blockfiles.Add(file);
-                }
-                return Directory.GetFiles(@"D:\С# примеры\Работа\SaveTest\").Length;
+                Dir = Directory.GetFileSystemEntries(path, "*", SearchOption.AllDirectories).Where(x => x.Contains(".")).ToList();
             }
             catch(Exception ex)
+            { return 0; }
+
+            var listNotConditions = (referens!=null && referens.Count > 0) ? Dir.Where(x => !referens.Any(d => x.Contains(d.Key)) && !referens.Any(d => x.Contains(d.Value))).ToList() : Dir;
+            //var listNotConditions = Dir.RemoveAll(x => referens.Any(d => x.Contains(d.Key)));
+
+            foreach (var file in listNotConditions)
             {
-                return 0;
+                GetArchive(file, savepath +  "ZipArchive_" + numbername.ToString(), out archivesize);
+                if (archivesize >= maxlengthzip) numbername++;
+            }
+
+            numbername++;
+            if (referens != null && referens.Count > 0)
+            {
+                foreach (var r in referens)
+                {
+                    var listWithConditions = Dir.Where(x => x.Contains(r.Key)).ToList();
+
+                    foreach (var list in listWithConditions)
+                    {
+                        var name = list.Replace(localpathdownload + "\\", "");
+                        name = name.Substring(name.LastIndexOf("\\")).Replace(".json", "");
+                        var block = Dir.Where(x => x.Contains(name) && x.Contains(r.Value)).ToList();
+
+                        GetArchive(list, savepath +  "ZipArchive_" + numbername.ToString(), out archivesize);
+                        foreach (var file in block)
+                        {
+                            GetArchive(file, savepath + "ZipArchive_" + numbername.ToString(), out archivesize);
+                        }
+
+                        if (archivesize >= maxlengthzip) numbername++;
+
+                    }
+                }
+            }
+
+
+            return Directory.GetFiles(savepath).Length;
+        }
+
+
+
+        private ZipArchive GetArchive(string path, string savename, out long lengtharchive)
+        {
+            using (var stream = new FileStream(savename + ".zip", FileMode.OpenOrCreate))
+            using (ZipArchive archive = new ZipArchive(stream, System.IO.Compression.ZipArchiveMode.Update))
+            {
+                var zipEntry = archive.CreateEntry(path.Replace(localpathdownload + @"\", ""));
+                using (Stream entryStream = zipEntry.Open())
+                {
+                    var blockbyte = File.ReadAllBytes(path);
+                    entryStream.Write(blockbyte, 0, blockbyte.Count());
+                }
+
+                lengtharchive = stream.Length;
+                return archive;
             }
         }
 
-        private ZipArchive GetArchive(List<string> path, string savename)
-        {
-            using (var stream = File.Create(@"D:\С# примеры\Работа\SaveTest\" + savename + ".zip"))
-            using (ZipArchive archive = new ZipArchive(stream, System.IO.Compression.ZipArchiveMode.Create))
-            {
-                foreach (var item in path)
-                {
-                    var zipEntry = archive.CreateEntry(item.Replace(localpathdownload + @"\", ""));
-                    using (Stream entryStream = zipEntry.Open())
-                    {
-                        var blockbyte = File.ReadAllBytes(item);
-                        entryStream.Write(blockbyte, 0, blockbyte.Count());
-                    }
-                }
-                return archive;
-            }
-           
-        }
     }
 }
--- DownLoadZip.Test/ZipFilesTest.cs
+++ DownLoadZip.Test/ZipFilesTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DownLoadZip.BLL.Services;
 using System.IO;
+using System.Collections.Generic;
 
 namespace DownLoadZip.Test
 {
@@ -12,18 +13,63 @@ namespace DownLoadZip.Test
         
 
         [TestMethod]
-        public void GetFiles()
+        public void GetArchivesNormalTest()
         {
             //arrange
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(@"D:\С# примеры\Работа\SaveTest\GetArchivesNormalTest\");
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
             Service service = new Service();
-            int countfiles = 113; //заданном размере 1024*100 и одном и томже архиве
+            Dictionary<string, string> referer = new Dictionary<string, string>()
+            {
+                ["Blog"] = "Article"
+            };
+            int countfiles = 37; //заданном размере 1024*100 и одном и томже архиве
             //act
-            var getallfiles = service.GetFiles(localpathdownload);           
+            var getallfiles = service.GetZipArchives(localpathdownload, 1024 * 100, referer, @"D:\С# примеры\Работа\SaveTest\GetArchivesNormalTest\");    
+
             //assert
             Assert.AreEqual(getallfiles, countfiles);
         }
 
 
+        [TestMethod]
+        public void GetArchives_Null_Comditioms_Test()
+        {
+            //arrange
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(@"D:\С# примеры\Работа\SaveTest\GetArchives_Null_Comditioms_Test\");
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+            Service service = new Service();
+            int countfiles = 38; //заданном размере 1024*100 и одном и томже архиве
+            //act
+            var getallfiles = service.GetZipArchives(localpathdownload, 1024 * 100, null, @"D:\С# примеры\Работа\SaveTest\GetArchives_Null_Comditioms_Test\");
+
+            //assert
+            Assert.AreEqual(getallfiles, countfiles);
+        }
+
+        [TestMethod]
+        public void GetArchives_Null_FilePath_Test()
+        {
+            //arrange
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(@"D:\С# примеры\Работа\SaveTest\GetArchives_Null_FilePath_Test\");
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+            Service service = new Service();
+            int countfiles = 0; //заданном размере 1024*100 и одном и томже архиве
+            //act
+            var getallfiles = service.GetZipArchives("", 1024 * 100, null, @"D:\С# примеры\Работа\SaveTest\GetArchives_Null_FilePath_Test\");
+
+            //assert
+            Assert.AreEqual(getallfiles, countfiles);
+        }
 
     }
 }
